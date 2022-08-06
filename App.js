fb853983fdc3ae7becf4714a6f6103656d655673
Shaun <scpour@gmail.com>
2022-08-06 11:52:27 -0400

    Nucampsite React Native #2

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
-import { Provider } from 'react-redux';
-import { store } from './Redux/store';
+import Main from './screens/MainComponent';
 import { NavigationContainer } from '@react-navigation/native';
-import Main from "./screens/MainComponent";
+import { Provider } from 'react-redux';
+import { persistor, store } from './Redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import Loading from './components/LoadingComponent';
 
 export default function App() {
-  return (
-    <Provider store={store}>
-      <NavigationContainer>
-          <Main />
-      </NavigationContainer>
-    </Provider>
-  );
+    return (
+        <Provider store={store}>
+            <PersistGate loading={<Loading />} persistor={persistor}>
+                <NavigationContainer>
+                    <Main />
+                </NavigationContainer>
+            </PersistGate>
+        </Provider>
+    );
 }
--- Redux/store.js
+++ Redux/store.js
@@ -1,16 +1,48 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit';
 import { campsitesReducer } from '../featured/campsites/campsitesSlice';
 import { commentsReducer } from '../featured/comments/commentsSlice';
 import { partnersReducer } from '../featured/partners/partnersSlice';
 import { promotionsReducer } from '../featured/promotions/promotionsSlice';
-import { favoritesReducer } from "../featured/favorites/favoritesSlice";
+import { favoritesReducer } from '../featured/favorites/favoritesSlice';
+import {
+    persistStore,
+    persistCombineReducers,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER
+} from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const config = {
+    key: 'root',
+    storage: AsyncStorage,
+    debug: true
+};
 
 export const store = configureStore({
-    reducer: {
+    reducer: persistCombineReducers(config, {
         campsites: campsitesReducer,
         comments: commentsReducer,
         partners: partnersReducer,
         promotions: promotionsReducer,
         favorites: favoritesReducer
-    }
+    }),
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [
+                    FLUSH,
+                    REHYDRATE,
+                    PAUSE,
+                    PERSIST,
+                    PURGE,
+                    REGISTER
+                ]
+            }
+        })
 });
+
+export const persistor = persistStore(store);
